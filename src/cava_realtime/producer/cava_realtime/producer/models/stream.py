3d95ec7c6196319a37a3987398bb762810ca42a9
Landung "Don" Setiawan <landungs@uw.edu>
2022-03-04 12:01:14 -0800

    Optimize producer for ooi data stream (#5)
    * Filter streams further and update task creation
    
    * Don't filter parameters and only push valid nums

--- src/cava_realtime/producer/cava_realtime/producer/models/stream.py
+++ src/cava_realtime/producer/cava_realtime/producer/models/stream.py
@@ -1,10 +1,7 @@
 import datetime
-import time
 import httpx
 import json
-import threading
 from loguru import logger
-from concurrent.futures import ThreadPoolExecutor
 from cava_realtime.producer.settings import producer_settings
 
 # logger = logging.getLogger(__name__)
@@ -58,6 +55,8 @@ class StreamProducer:
         self._begin_time = None
         self._kafka_producer = kafka_producer
         self.client = client
+        self.__paused = False
+        self.__no_data_count = 0
 
     def __repr__(self):
         return f"<Stream: {self.instrument_name} - {self.ref}>"
@@ -69,7 +68,7 @@ class StreamProducer:
         )
 
     def _display_status(self, status):
-        return f"{self.ref:<90}:{status:>50}"
+        return f"{status} ({self.ref})"
 
     async def _get_future_data(self):
         response = await self.client.send(self.request)
@@ -78,60 +77,74 @@ class StreamProducer:
     def _send_data(self, data=None):
         stream = {"ref": self.ref, "data": data}
         data_bytes = json.dumps(stream).encode("utf-8")
-        logger.info(self._display_status(f"Bytesize {len(data_bytes)}"))
         if self._kafka_producer is not None:
             # print data points returned
             self._kafka_producer.send(
                 self.topic, data_bytes, key=self.ref.encode("utf-8")
             )
         else:
-            logger.info(
+            logger.warning(
                 self._display_status(
                     "Data not sent anywhere. Kafka producer object not found."
                 )
             )
 
     def _extract_keys(self, data):
-        rdict = {key: [] for key in self.parameters}
+        rdict = {}
         min_time = self.last_time
         for record in data:
             if record['time'] <= min_time:
                 time_r = record['time']
                 time_r = ntp_seconds_to_datetime(time_r)
                 time_r = time_r.strftime("%Y-%m-%d %H:%M:%S.000Z")
-                logger.info(
-                    self._display_status(
-                        f'No new data found since {time_r}. Sending new request.'
-                    )
-                )
                 continue
-            for key in self.parameters:
-                rdict[key].append(record[key])
+            for key, value in record.items():
+                # Only save floats and ints
+                if any(isinstance(value, dtype) for dtype in [int, float]):
+                    rdict.setdefault(key, [])
+                    rdict.get(key).append(value)
         logger.info(
             self._display_status(
-                f"Found {len(rdict['time'])} new data points after filtering"
+                f"Found {len(rdict['time'])} new data points since {self._begin_time}"
             )
         )
         return rdict
 
     async def request_data(self):
-        logger.info(
-            self._display_status(
-                f"Last requested: {ntp_seconds_to_datetime(self.last_time)}"
+        if self.__no_data_count == 1:
+            self.__paused = True
+
+        if self.__paused is True:
+            # If the stream requests are paused
+            # for a day, then restart it
+            time_since_request = (
+                datetime.datetime.utcnow()
+                - self.last_time
             )
-        )
-        self._begin_time = datetime.datetime.utcnow() - datetime.timedelta(
-            seconds=10
-        )
+            if time_since_request >= datetime.timedelta(days=1):
+                self.__paused = False
+                self.__no_data_count = 0
+                self.last_time = 0
+                pass
+            else:
+                return None
+
+        if self._begin_time is None:
+            self._begin_time = datetime.datetime.utcnow() - datetime.timedelta(
+                seconds=10
+            )
         begin_time_str = self._begin_time.strftime('%Y-%m-%dT%H:%M:%S.000Z')
 
         self._params['beginDT'] = begin_time_str
         # request complete, if not 200, log error and try again
         response = await self._get_future_data()
-        if response.status_code != 200:
+        if response.status_code != httpx.codes.OK:
             logger.warning(
-                self._display_status(str(response.json()['message']['status']))
+                self._display_status("No data are available.")
             )
+            self.__no_data_count += 1
+            self.last_time = self._begin_time
+            self._begin_time = None
             return None
         try:
             # store json response
--- src/cava_realtime/producer/cava_realtime/producer/settings.py
+++ src/cava_realtime/producer/cava_realtime/producer/settings.py
@@ -1,8 +1,10 @@
 from pydantic import BaseSettings
+from typing import List
 
 
 class ProducerSettings(BaseSettings):
     """Application settings."""
+
     development: bool = False
 
     # OOI M2M Settings
@@ -15,6 +17,22 @@ class ProducerSettings(BaseSettings):
 
     # CAVA API Settings
     metadata_url: str = 'https://api-development.ooica.net/metadata'
+
+    # Dev Instruments
+    dev_instruments: List[str] = [
+        "RS03AXBS-LJ03A-12-CTDPFB301-streamed-ctdpf_optode_sample",
+        "RS01SLBS-LJ01A-12-CTDPFB101-streamed-ctdpf_optode_sample",
+        "RS01SBPS-PC01A-4A-CTDPFA103-streamed-ctdpf_optode_sample",
+        "RS03AXPS-SF03A-2A-CTDPFA302-streamed-ctdpf_sbe43_sample",
+        "RS03AXPS-PC03A-4A-CTDPFA303-streamed-ctdpf_optode_sample",
+        "RS01SBPS-SF01A-2A-CTDPFA102-streamed-ctdpf_sbe43_sample",
+        "CE02SHBP-LJ01D-06-CTDBPN106-streamed-ctdbp_no_sample",
+        "CE04OSBP-LJ01C-06-CTDBPO108-streamed-ctdbp_no_sample",
+        "RS03AXPS-PC03A-4C-FLORDD303-streamed-flort_d_data_record",
+        "RS03AXPS-SF03A-3A-FLORTD301-streamed-flort_d_data_record",
+        "RS01SBPS-SF01A-3A-FLORTD101-streamed-flort_d_data_record",
+        "RS01SBPS-PC01A-4C-FLORDD103-streamed-flort_d_data_record",
+    ]
 
     class Config:
         """model config"""
